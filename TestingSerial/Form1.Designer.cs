38170b338d43d244bbbbfbb51ea3b51bad70c773
martrewes <martinweston90@gmail.com>
2021-03-25 18:32:22 +0000

    Add project files.

---
+++ TestingSerial/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace TestingSerial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSend = new System.Windows.Forms.Button();
+            this.lstPorts = new System.Windows.Forms.ListBox();
+            this.txtString = new System.Windows.Forms.TextBox();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(281, 49);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(81, 47);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // lstPorts
+            // 
+            this.lstPorts.FormattingEnabled = true;
+            this.lstPorts.ItemHeight = 20;
+            this.lstPorts.Location = new System.Drawing.Point(12, 12);
+            this.lstPorts.Name = "lstPorts";
+            this.lstPorts.Size = new System.Drawing.Size(120, 84);
+            this.lstPorts.TabIndex = 1;
+            this.lstPorts.SelectedIndexChanged += new System.EventHandler(this.lstPorts_SelectedIndexChanged);
+            // 
+            // txtString
+            // 
+            this.txtString.Location = new System.Drawing.Point(12, 102);
+            this.txtString.Name = "txtString";
+            this.txtString.Size = new System.Drawing.Size(350, 26);
+            this.txtString.TabIndex = 2;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(139, 13);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 41);
+            this.btnRefresh.TabIndex = 3;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 161);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.txtString);
+            this.Controls.Add(this.lstPorts);
+            this.Controls.Add(this.btnSend);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.ListBox lstPorts;
+        private System.Windows.Forms.TextBox txtString;
+        private System.Windows.Forms.Button btnRefresh;
+    }
+}
+
---
+++ TestingSerial/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+
+
+
+namespace TestingSerial
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+
+        public void RefreshPorts()
+        {
+            lstPorts.Items.Clear();
+            string[] ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+            {                
+                lstPorts.Items.Add(port);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RefreshPorts();
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            RefreshPorts();
+        }
+
+        public void btnSend_Click(object sender, EventArgs e)
+        {
+            if (lstPorts.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select an Item first!");
+            }
+            else { 
+                string strPort = lstPorts.SelectedItem.ToString();
+                string strSend = txtString.Text;
+                int baud = 9600;
+                var sendPort = new SerialPort(strPort, baud);
+                sendPort.Open();
+                sendPort.WriteLine(strSend);
+                sendPort.Close();
+            }
+        }
+
+        public void lstPorts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+
+}

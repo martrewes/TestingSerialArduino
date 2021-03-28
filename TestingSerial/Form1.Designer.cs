8ea70bc99628f20605432f34bbd3f585ad85a14b
martrewes <martinweston90@gmail.com>
2021-03-29 00:55:01 +0100

    Now somewhat funtioning

--- TestingSerial/Form1.Designer.cs
+++ TestingSerial/Form1.Designer.cs
@@ -33,6 +33,8 @@ namespace TestingSerial
             this.lstPorts = new System.Windows.Forms.ListBox();
             this.txtString = new System.Windows.Forms.TextBox();
             this.btnRefresh = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnSend
@@ -72,11 +74,32 @@ namespace TestingSerial
             this.btnRefresh.UseVisualStyleBackColor = true;
             this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
             // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(13, 135);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(235, 124);
+            this.listBox1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(255, 135);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(391, 161);
+            this.ClientSize = new System.Drawing.Size(391, 286);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.btnRefresh);
             this.Controls.Add(this.txtString);
             this.Controls.Add(this.lstPorts);
@@ -95,6 +118,8 @@ namespace TestingSerial
         private System.Windows.Forms.ListBox lstPorts;
         private System.Windows.Forms.TextBox txtString;
         private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- TestingSerial/Form1.cs
+++ TestingSerial/Form1.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -14,17 +16,67 @@ using System.IO.Ports;
 
 namespace TestingSerial
 {
-    
+  
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
-            
+            var watcher = new FileSystemWatcher(@"D:\Desktop\")
+            {
+                NotifyFilter = NotifyFilters.Attributes
+                                 | NotifyFilters.CreationTime
+                                 | NotifyFilters.DirectoryName
+                                 | NotifyFilters.FileName
+                                 | NotifyFilters.LastAccess
+                                 | NotifyFilters.LastWrite
+                                 | NotifyFilters.Security
+                                 | NotifyFilters.Size
+            };
+
+            watcher.Changed += OnChanged;
+
+            watcher.Filter = "fooNow.txt";
+            watcher.IncludeSubdirectories = true;
+            watcher.EnableRaisingEvents = true;
+
+            Console.WriteLine("Press enter to exit.");
+            Console.ReadLine();
         }
 
+        private static void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            if (e.ChangeType != WatcherChangeTypes.Changed)
+            {   
 
-        public void RefreshPorts()
+            }
+            System.Threading.Thread.Sleep(200);
+            String songInfo = "D:\\Desktop\\fooNow.txt";  
+                String strPort = "COM3";
+                String strSend = "|";
+                string line;
+                int counter = 0;
+                System.IO.StreamReader lineReader = new System.IO.StreamReader(songInfo);
+                while ((line = lineReader.ReadLine()) != null)
+                {
+                    strSend = strSend + line + "|";
+                    counter++;
+                }
+                lineReader.Close();
+                int baud = 9600;
+                var sendPort = new SerialPort(strPort, baud);
+                sendPort.Open();
+                sendPort.WriteLine(strSend);
+                sendPort.Close();
+            
+            Console.WriteLine($"Changed: {e.FullPath}");
+        }
+
+    
+
+
+    public void RefreshPorts()
         {
             lstPorts.Items.Clear();
             string[] ports = SerialPort.GetPortNames();
@@ -46,22 +98,15 @@ namespace TestingSerial
 
         public void btnSend_Click(object sender, EventArgs e)
         {
-            if (lstPorts.SelectedIndex == -1)
-            {
-                MessageBox.Show("Please select an Item first!");
-            }
-            else { 
-                string strPort = lstPorts.SelectedItem.ToString();
-                string strSend = txtString.Text;
-                int baud = 9600;
-                var sendPort = new SerialPort(strPort, baud);
-                sendPort.Open();
-                sendPort.WriteLine(strSend);
-                sendPort.Close();
-            }
+            
         }
 
         public void lstPorts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
         {
             
         }
